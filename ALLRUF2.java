e1c64b7de3a4b18c9e2e39575bb43cf7bc79fb4e
MAHESH CHEEMA <124231713+CheemaMahesh@users.noreply.github.com>
2023-07-28 01:39:31 +0530

    Add files via upload

---
+++ ALLRUF2.java
@@ -0,0 +1,23 @@
+package dUMMYFILE;
+
+public class ALLRUF2 {
+	public static void main(String[] args) 
+	{ 
+	int	[][] arr = { {1,2,4,5,7},{3,4,5,6,7},{5,6,7,8,9} }; 
+	    System.out.println(arr.length+arr[0].length);
+	}
+//        int [] arr = new int [] {1, 2, 3, 4, 5};  
+//          
+//        for (int i = 0; i < arr.length; i++) {  
+//            System.out.print(arr[i] + " ");  
+//        }  
+//        System.out.println(); 
+//        
+//         
+//        for (int i = arr.length-1; i >= 0; i--) {  
+//            System.out.print(arr[i] + " ");  
+//        }  
+    }  
+
+
+
